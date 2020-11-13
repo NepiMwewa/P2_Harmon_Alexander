5b4236046cb5c9a33ebc149cf42a98abb2b8667a
NepiMwewa <wolfaceking@gmail.com>
2020-11-13 10:32:24 -0800

    Added text shadows and box shadows

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ window.onload = init;
 function init() {
   var showcase = document.getElementById("showcase_picture");
 
-  var nextButton = document.getElementById("next_button");
-  var previousButton = document.getElementById("previous_button");
+  var nextSlide = document.getElementById("next_button");
+  var previousSlide = document.getElementById("previous_button");
 
-  nextButton.onclick = function(){
-    showcase.src = "images/game_1.png";
+
+  nextSlide.onclick = function(){
+    showcase.src = "images/showcase_1.png";
   }
 
-  previousButton.onclick = function(){
-    showcase.src = "images/game_2.png";
+  previousSlide.onclick = function(){
+    showcase.src = "images/mobile_showcase_1.png";
   }
 }
