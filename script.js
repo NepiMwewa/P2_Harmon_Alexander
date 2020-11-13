5f670738b4fa91410beccdcd2e292d6f3e5c99e1
NepiMwewa <wolfaceking@gmail.com>
2020-11-12 22:32:05 -0800

    Added javascript to the showcase and buttons. work in progress.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.onload = init;
 
+function init() {
+  var showcase = document.getElementById("showcase_picture");
+
+  var nextButton = document.getElementById("next_button");
+  var previousButton = document.getElementById("previous_button");
+
+  nextButton.onclick = function(){
+    showcase.src = "images/game_1.png";
+  }
+
+  previousButton.onclick = function(){
+    showcase.src = "images/game_2.png";
+  }
+}
