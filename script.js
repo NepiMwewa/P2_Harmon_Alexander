1a3c6ed954fdeb9b714bf7bb9bc2ac45055099f8
NepiMwewa <wolfaceking@gmail.com>
2020-11-15 12:07:34 -0800

    added new mobile and desktop images. added JS so the correct image will be displayed based on width

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ function init() {
   var nextSlide = document.getElementById("next_button");
   var previousSlide = document.getElementById("previous_button");
 
+  var windowSize = window.innerWidth;
+
+  if(windowSize < 768){
+    showcase.src = "images/mobile_showcase_1.png";
+  } else if(windowSize < 1024) {
+    showcase.src = "images/mobile_showcase_1.png";
+  } else {
+    showcase.src = "images/showcase_1.png";
+  }
 
   nextSlide.onclick = function(){
     showcase.src = "images/showcase_1.png";
   }
 
   previousSlide.onclick = function(){
-    showcase.src = "images/mobile_showcase_1.png";
+    showcase.src = "images/showcase_2.png";
   }
 }
